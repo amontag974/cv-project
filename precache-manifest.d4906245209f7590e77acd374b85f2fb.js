230cf755fce0046a0a10e1e44db0ab9953cb1846
Alex <amontag974@gmail.com>
2020-12-15 14:38:45 -0500

    Updates

---
+++ precache-manifest.d4906245209f7590e77acd374b85f2fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9143cb4ffc4efd639190cb9e880889a2",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "4ad00b2dbb8bd364cfa7",
+    "url": "/cv-project/static/css/main.30d6f3f3.chunk.css"
+  },
+  {
+    "revision": "0ad6866bec61817dfd9c",
+    "url": "/cv-project/static/js/2.3b32808e.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/cv-project/static/js/2.3b32808e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ad00b2dbb8bd364cfa7",
+    "url": "/cv-project/static/js/main.f5f6cbb8.chunk.js"
+  },
+  {
+    "revision": "f1ebb3293288666c5950",
+    "url": "/cv-project/static/js/runtime-main.d91c792d.js"
+  }
+]);
